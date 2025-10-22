2f03af1cec4bd2637593721aec3e691c02e50597
Aru_0077 <285309697@qq.com>
2025-10-22 15:33:49 +0800

    home page

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,8 +1,17 @@
 import { createRouter, createWebHistory } from 'vue-router'
 
+// 导入首页组件
+import Home from '@/views/home/HomePage.vue'
+
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
-  routes: [],
+  routes: [
+    {
+      path: '/', // 路由路径
+      name: 'Home', // 路由名称
+      component: Home, // 对应的组件
+    },
+  ],
 })
 
 export default router
---
+++ src/views/home/HomePage.vue
@@ -0,0 +1,22 @@
+
+<!-- src/views/home/home.vue -->
+<template>
+  <div class="home-container">
+    <h2>首页</h2>
+    <p>这是一个简单的首页页面</p>
+  </div>
+</template>
+
+<script setup lang="ts">
+
+
+
+
+</script>
+
+<style scoped>
+.home-container {
+  padding: 20px;
+  text-align: center;
+}
+</style>
